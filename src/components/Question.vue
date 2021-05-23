e7745f28e959216e4f32e9061fd429da1e97e9bb
unknown <jhir3673@uni.sydney.edu.au>
2021-05-24 09:52:31 +1000

    Created the Question component and Question Page and made active

---
+++ src/components/Question.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{question.qName}}</h2>
+    <p>{{question.qParagraphs}}</p>
+    <q-option-group
+      :options="question.qOptions"
+      label="Answers"
+      type="radio"
+      v-model="group"
+      />
+  </div>
+</template>
+<script>
+export default {
+  Name: 'Question',
+  props: {
+    question: Object
+  },
+  data () {
+    return {
+      group: null
+    }
+  }
+}
+</script>
---
+++ src/pages/student/Questions.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    <Question v-bind:question="questions[0]"/>
+  </q-page>
+</template>
+
+<script>
+import Question from '../../components/Question.vue'
+export default {
+  components: { Question },
+  name: 'Questions',
+  data () {
+    return {
+      questions: [{
+        ID: 0,
+        qName: 'test',
+        qParagraphs: 'This is a test paragraph',
+        qOptions: [{
+          ID: 0,
+          label: 'This is the correct option',
+          value: 'corr'
+        },
+        {
+          ID: 1,
+          label: 'This is the incorrect option',
+          value: 'incorr'
+        }]
+      }]
+    }
+  }
+}
+</script>
